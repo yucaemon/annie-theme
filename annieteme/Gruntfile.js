d9c59983d7eb6bbe6f5a43aa4d29e20185616b0d
Yuka Suzuki <suzukiyuca@gmail.com>
2017-12-05 16:24:55 -0800

    sass,hamlのビルド設定（sass,haml）

---
+++ annieteme/Gruntfile.js
@@ -0,0 +1,56 @@
+/**
+ * Created by yuka on 12/5/17.
+ */
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        //package.jsonを取得
+        pkg: grunt.file.readJSON('package.json'),
+
+        //タスクの設定（grunt-contrib-sass）
+        sass: {
+            styles: {
+                //SCSSファイルを指定
+                src: '_scss/style.scss',
+                //CSSを書きだしファイルを指定
+                dest: 'css/style.css'
+            }
+        },
+
+        //タスクの設定（grunt-contrib-watch）
+        watch: {
+            sass: {
+                //変更を監視するファイルを指定
+                files: ['_scss/*.scss'],
+                //変更されたらどのタスクを実行するか指定
+                tasks: [ 'sass' ]
+            },
+            haml: {
+                //変更を監視するファイルを指定
+                files: ['*.haml'],
+                //変更されたらどのタスクを実行するか指定
+                tasks: [ 'haml' ]
+            }
+        },
+
+        haml: {
+            dist: {
+                files: {
+                    'main.html': 'main.haml'
+                }
+            }
+        }
+
+
+
+    });
+
+    // プラグインの読み込み
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-haml');
+
+    grunt.registerTask('default', [ 'sass', 'haml' ]);
+
+};
