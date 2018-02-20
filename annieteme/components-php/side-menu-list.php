faf3e2017188e8d83fd9f8b7a04d347283d35997
Yuka Suzuki <suzukiyuca@gmail.com>
2018-02-20 01:49:25 -0800

    index-page : wp data get for side menu part

--- annieteme/components-php/side-menu-list.php
+++ annieteme/components-php/side-menu-list.php
@@ -1,118 +1,26 @@
 <div class='side-menu-list'>
-<dl>
-<dt class='side-menu-list__title'>
+<div class='side-menu-list__li'>
+<p class='side-menu-list__title'>
 記事
-</dt>
-<dd>
-<span class='arrow'>
->
-</span>
-ファッション
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-ライフスタイル
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-ビューティー
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-インテリア
-</dd>
-</dl>
-<dl>
-<dt class='side-menu-list__title'>
-購入可能アイテム
-</dt>
-<dd>
-<span class='arrow'>
->
-</span>
-全て
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-ボトムス
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-上着
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-雑貨
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-コスメ
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-アクセサリー
-</dd>
-</dl>
-<dl>
-<dt class='side-menu-list__title'>
+</p>
+<?php wp_list_categories('child_of=3&title_li='); ?>
+</div>
+<div class='side-menu-list__li'>
+<p class='side-menu-list__title'>
+購入アイテム
+</p>
+<?php wp_list_categories('child_of=7&title_li='); ?>
+</div>
+<div class='side-menu-list__li'>
+<p class='side-menu-list__title'>
 日本未上陸ブランド
-</dt>
-<dd>
-<span class='arrow'>
->
-</span>
-genuine People
-<div class='japanese'>
-ジェーニーピーポー
+</p>
+<?php wp_list_categories('child_of=15&title_li='); ?>
 </div>
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-threebirdnest
-<div class='japanese'>
-スリーバードネスト
-</div>
-</dd>
-</dl>
-<dl>
-<dt class='side-menu-list__title'>
+<div class='side-menu-list__li'>
+<p class='side-menu-list__title'>
 ポートランドブランド
-</dt>
-<dd>
-<span class='arrow'>
->
-</span>
-genuine People
-<div class='japanese'>
-ジェーニーピーポー
+</p>
+<?php wp_list_categories('child_of=18&title_li='); ?>
 </div>
-</dd>
-<dd>
-<span class='arrow'>
->
-</span>
-threebirdnest
-<div class='japanese'>
-スリーバードネスト
-</div>
-</dd>
-</dl>
 </div>
