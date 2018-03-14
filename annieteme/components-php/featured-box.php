dcd53cecd58db23c38d728c3a53cddc903288d42
Yuka Suzuki <suzukiyuca@gmail.com>
2018-03-14 01:44:42 -0700

    index-page : get wp to side of recommend part.

--- annieteme/components-php/featured-box.php
+++ annieteme/components-php/featured-box.php
@@ -1,11 +1,24 @@
 <div class='featured-box'>
-<h3 class='featured-box__header'>
-未上陸ブランドを購入しよう！冬の『BOHO』ファッション特集
-</h3>
-<div class='featured-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/side-featured.png" width="312">
-</div>
-<p class='featured-box__read'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</p>
+<?php $args = array(
+  'numberposts' => 1,                //表示（取得）する記事の数
+  'post_type' => 'featured'    //投稿タイプの指定
+);
+$posts = get_posts( $args );
+if( $posts ) : foreach( $posts as $post) : setup_postdata( $post ); ?>
+  <h3 class="featured-box__header">
+    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+  </h3>
+  <div class="featured-img">
+    <?php if ( has_post_thumbnail() ) : ?>
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+    <?php endif; ?>
+  </div>
+  <p class="featured-box__read">
+    <a href="<?php the_permalink(); ?>"><?php echo mb_substr(strip_tags($post-> post_content),0,200) ; ?></a>
+  </p>
+<?php endforeach; ?>
+<?php else : //記事が無い場合 ?>
+  <li><p>記事はまだありません。</p></li>
+<?php endif;
+wp_reset_postdata(); //クエリのリセット ?>
 </div>
--- annieteme/components-php/recommend-list.php
+++ annieteme/components-php/recommend-list.php
@@ -4,59 +4,24 @@
 <img src="<?php echo get_template_directory_uri(); ?>/images/recommend.svg">
 </div>
 </dt>
-<dd class='news-list__content flexbox'>
-<div class='news-list__number-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/1.svg">
-</div>
-<div class='news-list__thumb-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/side-test1.png">
-</div>
-<p class='news-list__title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</p>
-</dd>
-<dd class='news-list__content flexbox'>
-<div class='news-list__number-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/2.svg">
-</div>
-<div class='news-list__thumb-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/side-test1.png">
-</div>
-<p class='news-list__title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</p>
-</dd>
-<dd class='news-list__content flexbox'>
-<div class='news-list__number-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/3.svg">
-</div>
-<div class='news-list__thumb-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/side-test1.png">
-</div>
-<p class='news-list__title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</p>
-</dd>
-<dd class='news-list__content flexbox'>
-<div class='news-list__number-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/4.svg">
-</div>
-<div class='news-list__thumb-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/side-test1.png">
-</div>
-<p class='news-list__title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</p>
-</dd>
-<dd class='news-list__content flexbox'>
-<div class='news-list__number-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/5.svg">
-</div>
-<div class='news-list__thumb-img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/side-test1.png">
-</div>
-<p class='news-list__title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</p>
-</dd>
+<?php
+$posts = get_posts(array(
+'posts_per_page' => 5, // 表示件数
+'category' => '21' // カテゴリIDもしくはスラッグ名
+));
+?>
+<?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
+
+  <!--表示する内容が入ります。-->
+  <dd class="news-list__content flexbox">
+    <div class="news-list__thumb-img">
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+    </div>
+    <p class="news-list__title">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </p>
+  </dd>
+  <!--表示する内容ここまで-->
+
+<?php endforeach; endif; ?>
 </dl>
