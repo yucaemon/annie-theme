d0903f92c3a8396150e6c74abc24cbdf525c6e2f
Yuka Suzuki <suzukiyuca@gmail.com>
2018-03-25 20:01:08 -0700

    index-page : get wp to side of keywords part.

--- annieteme/components-php/keywords.php
+++ annieteme/components-php/keywords.php
@@ -3,20 +3,6 @@
 <img src="<?php echo get_template_directory_uri(); ?>/images/keywords.svg">
 </div>
 <div class='keywords__content flexbox'>
-<div class='keywords__tag'>
-古着
-</div>
-<div class='keywords__tag'>
-冬物コート
-</div>
-<div class='keywords__tag'>
-日本未上陸ブランド
-</div>
-<div class='keywords__tag'>
-ブーツ
-</div>
-<div class='keywords__tag'>
-ルームウェア
-</div>
+<?php wp_tag_cloud( 'smallest=11&largest=11' ); ?>
 </div>
 </div>
