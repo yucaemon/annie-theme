b689a78e362841875bee69e34ddb5692a8b48432
Yuka Suzuki <suzukiyuca@gmail.com>
2018-05-14 23:59:54 -0700

    making tag page - get tag content deta

--- annieteme/components-php/header.php
+++ annieteme/components-php/header.php
@@ -12,7 +12,7 @@
 <img src="<?php echo get_template_directory_uri(); ?>/images/annie-logo.svg" width="250">
 </div>
 <h1 class='sub-title'>
-ポートランドから、自由奔放『BOHO』ファッションをお届けするメディア
+ポートランドから、自由奔放『BOHO』ファッションをお届け。海外通販メディア
 </h1>
 </div>
 <div class='info'>
--- annieteme/tag.php
+++ annieteme/tag.php
@@ -10,202 +10,25 @@
 </ul>
 <div class='archive-banner'>
 <h1 class='archive-banner__header'>
-<?php echo get_the_archive_title(); ?> に関する記事一覧    　
+<?php echo get_the_archive_title(); ?> に関する記事一覧
 </h1>
 </div>
 <div class='outer__inner flexbox--h-center flexbox--spacearound'>
 <?php include('components-php/side-menu-list.php'); ?>
 <div class='column-6 middle-contain'>
-<ul class='article-list'>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample1.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample2.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample3.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample1.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample2.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample3.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample1.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample2.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
-<li class='flexbox'>
-<div class='article-list__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/sample3.png">
-</div>
-<div class='article-list__text'>
-<h3 class='list-title'>
-bohoスタイルの火付け役『アンソロジー』のゆるふあチェックカーデが可愛い。
-</h3>
-<p class='list-read'>
-ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。ここ人は本文が入ります。
-</p>
-<div class='icons flexbox'>
-<div class='icons__tag'>
-購入可能
-</div>
-<div class='icons__tag'>
-日本未上陸
-</div>
-</div>
-</div>
-</li>
+<ul class="article-list">
+<?php if(have_posts()): while(have_posts()):the_post(); ?>
+  <li class='flexbox'>
+  <?php if( has_post_thumbnail() ): ?>
+  <?php the_post_thumbnail( 'post-thumbnails', array('class' => 'article-list__img') ); ?>
+  <div class="article-list__text">
+    <h3 class="list-title"><?php echo mb_substr($post->post_title, 0, 20).'…'; ?></h3>
+    <p class='list-read'><?php echo mb_substr(strip_tags($post-> post_content),0,100) ; ?></p>
+  </div>
+　<?php else: ?>
+　<?php endif; ?>
+  </li>
+<?php endwhile; endif; ?>
 </ul>
 <?php include('components-php/more-search-article.php'); ?>
 <?php include('components-php/sns-box.php'); ?>
