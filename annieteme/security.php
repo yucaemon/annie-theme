858a60bd09d453cd5935abb68ad33079486edfef
Yuka Suzuki <suzukiyuca@gmail.com>
2018-05-24 19:25:58 -0700

    done for security page

--- annieteme/security.php
+++ annieteme/security.php
@@ -28,9 +28,9 @@ ANNIE Jeneは、以下のとおり情報セキュリティ基本方針を定め�
 </p>
 </div>
 </div>
-<?php include('components-php/related-article.php'); ?>
+<?php include('components-php/news-articles.php'); ?>
 <?php include('components-php/more-search-article.php'); ?>
-<div class='column-8 margin-center'>
+<div class='column-6 margin-center'>
 <?php include('components-php/sns-box.php'); ?>
 </div>
 <div class='column-8 margin-center'>
