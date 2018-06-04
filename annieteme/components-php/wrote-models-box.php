c7ed58f20576b7bff8b6c4b5d266fe94c68eea71
Yuka Suzuki <suzukiyuca@gmail.com>
2018-06-04 16:04:59 -0700

    modification layout for wrote-models-box

--- annieteme/components-php/wrote-models-box.php
+++ annieteme/components-php/wrote-models-box.php
@@ -1,19 +1,10 @@
-<div class='instagram-models-lists'>
-<div class='instagram-models-lists__container'>
-<div class='instagram-models-lists__header'>
-CREATED BY
-</div>
-<div class='instagram-models-lists__models'></div>
+<div class='wrote-models-lists'>
 <?php
 $user = wp_get_current_user();
 if ( $user ) :
 ?>
-<img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" />
-
+<div class="wrote-models-lists__header">CREATED BY </div>
+<div class="wrote-models-lists__img"><img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" /></div>
 <?php echo my_get_the_post_profile(); ?>
-
-
-
 <?php endif; ?>
-</div>
 </div>
