97c36b15ecbffbef9e0f82815396e3bc535e27cf
Yuka Suzuki <suzukiyuca@gmail.com>
2018-06-05 13:06:37 -0700

    modification for header

--- annieteme/components-php/header.php
+++ annieteme/components-php/header.php
@@ -1,13 +1,15 @@
 <header class='outer__header header'>
 <div class='flexbox--spacebetween'>
-<?php include('sns-icons.php'); ?>
-<div class='logo'>
-<div class='logo__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/annie-logo.svg" width="250">
+<div class='sns-icons flexbox'>
+<div class='sns-icons__icon'>
+<a href="https://www.facebook.com/Annie-Jene-226289061241623/"><img src="<?php echo get_template_directory_uri(); ?>/images/facebook-icon.svg"></a>
 </div>
-<h1 class='sub-title'>
-ポートランドから、自由奔放『BOHO』ファッションをお届け。海外通販メディア
-</h1>
+<div class='sns-icons__icon'>
+<a href="https://twitter.com/bohostyle_love"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter-icon.svg"></a>
+</div>
+<div class='sns-icons__icon'>
+<a href="https://www.instagram.com/annie_jene/"><img src="<?php echo get_template_directory_uri(); ?>/images/instagram-icon.svg"></a>
+</div>
 </div>
 <div class='info'>
 <ul class='nav-menu flexbox'>
@@ -19,8 +21,18 @@
 </li>
 </ul>
 <div class='search__box'>
+<div class='search__form'>
 <?php get_search_form(); ?>
 </div>
 </div>
+</div>
+</div>
+<div class='logo'>
+<div class='logo__img'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/annie-logo.svg" width="250">
+</div>
+<h1 class='sub-title'>
+ポートランドから、自由奔放『BOHO』ファッションをお届け。海外通販メディア
+</h1>
 </div>
 </header>
--- annieteme/components-php/header02.php
+++ annieteme/components-php/header02.php
@@ -1,6 +1,16 @@
 <header class='outer__header header02'>
 <div class='flexbox--spacebetween'>
-<?php include('sns-icons.php'); ?>
+<div class='sns-icons flexbox'>
+<div class='sns-icons__icon'>
+<a href="https://www.facebook.com/Annie-Jene-226289061241623/"><img src="<?php echo get_template_directory_uri(); ?>/images/facebook-icon.svg"></a>
+</div>
+<div class='sns-icons__icon'>
+<a href="https://twitter.com/bohostyle_love"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter-icon.svg"></a>
+</div>
+<div class='sns-icons__icon'>
+<a href="https://www.instagram.com/annie_jene/"><img src="<?php echo get_template_directory_uri(); ?>/images/instagram-icon.svg"></a>
+</div>
+</div>
 <div class='info'>
 <ul class='nav-menu flexbox'>
 <li>
